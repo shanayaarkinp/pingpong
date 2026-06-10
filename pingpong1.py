2197f7de9ffa1696d9c962199dd98e1799ee67bb
shanayaarkinp <shanayapunongbayan@gmail.com>
2026-06-10 16:25:33 +0800

    Add files via upload
    v1.0

---
+++ pingpong1.py
@@ -0,0 +1,46 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 45
+
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+
+        ball.update()
+        ball.reset()
+
+
+    display.update()
+    clock.tick(FPS)
+
+quit()
